da238cb8e7e0aec527c82d9979d88b0713b588d1
awwm <wahab83.pk@gmail.com>
2023-07-08 20:10:50 +0200

    add odds comparison plugin structure in repo

---
+++ wp-content/plugins/odds-comparison/admin/partials/oc-admin-display.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       https://www.linkedin.com/in/abdulwahabpk/
+ * @since      1.0.0
+ *
+ * @package    Oc
+ * @subpackage Oc/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
+
+<div class="wrap">
+    <h2>Odds Comparison Plugin Settings</h2>
+</div>
---
+++ wp-content/plugins/odds-comparison/includes/class-oc-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://www.linkedin.com/in/abdulwahabpk/
+ * @since      1.0.0
+ *
+ * @package    Oc
+ * @subpackage Oc/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Oc
+ * @subpackage Oc/includes
+ * @author     Abdul Wahab <wahab83.pk@gmail.com>
+ */
+class Oc_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
