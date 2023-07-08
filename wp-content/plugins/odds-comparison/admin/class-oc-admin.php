a383a9afb2fe4c81253848843c59ad461d00b7c1
awwm <wahab83.pk@gmail.com>
2023-07-08 23:38:03 +0200

    Add plugin admin settings page with test input field

--- wp-content/plugins/odds-comparison/admin/class-oc-admin.php
+++ wp-content/plugins/odds-comparison/admin/class-oc-admin.php
@@ -52,7 +52,8 @@ class Oc_Admin {
 		$this->plugin_name = $plugin_name;
 		$this->version = $version;
 
-		add_action('admin_menu', array( $this, 'addPluginAdminMenu' ), 9); 
+		add_action('admin_menu', array( $this, 'addPluginAdminMenu' ), 9);
+		add_action('admin_init', array( $this, 'registerAndBuildFields' ));
 	}
 
 	/**
@@ -108,11 +109,117 @@ class Oc_Admin {
 	 */
 	public function addPluginAdminMenu() {
 		//add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );
-		add_menu_page(  $this->plugin_name, 'Odds Comparison', 'administrator', $this->plugin_name, array( $this, 'displayPluginAdminDashboard' ), 'dashicons-chart-area', 26 );
+		add_menu_page(  $this->plugin_name, 'Odds Comparison', 'administrator', $this->plugin_name, array( $this, 'displayPluginAdminSettings' ), 'dashicons-chart-area', 26 );
 	}
 
-	public function displayPluginAdminDashboard() {
+	public function displayPluginAdminSettings() {
+		// set this var to be used in the settings-display view
+		$active_tab = isset( $_GET[ 'tab' ] ) ? $_GET[ 'tab' ] : 'general';
+		if(isset($_GET['error_message'])){
+			add_action('admin_notices', array($this,'pluginAdminSettingsMessages'));
+			do_action( 'admin_notices', $_GET['error_message'] );
+		}
 		require_once 'partials/'.$this->plugin_name.'-admin-display.php';
-  	}
+	}
+
+	public function pluginAdminSettingsMessages($error_message){
+		switch ($error_message) {
+			case '1':
+				$message = __( 'There was an error adding this setting. Please try again.  If this persists, shoot us an email.', 'oc' );                 
+				$err_code = esc_attr( 'oc_admin_setting' );                 
+				$setting_field = 'oc_admin_setting';                 
+			break;
+		}
+		$type = 'error';
+		add_settings_error(
+			$setting_field,
+			$err_code,
+			$message,
+			$type
+		);
+	}
+
+	public function registerAndBuildFields() {
+		/**
+		 * First, we add_settings_section. This is necessary since all future settings must belong to one.
+		 * Second, add_settings_field
+		 * Third, register_setting
+		 */     
+		add_settings_section(
+			// ID used to identify this section and with which to register options
+			'oc_general_section', 
+			// Title to be displayed on the administration page
+			'',  
+			// Callback used to render the description of the section
+			array( $this, 'oc_display_general_account' ),    
+			// Page on which to add this section of options
+			'oc_general_settings'                   
+		);
+		unset($args);
+		$args = array (
+			'type'      => 'input',
+			'subtype'   => 'text',
+			'id'    => 'oc_admin_setting',
+			'name'      => 'oc_admin_setting',
+			'required' => 'true',
+			'get_options_list' => '',
+			'value_type'=>'normal',
+			'wp_data' => 'option'
+		);
+		add_settings_field(
+			'oc_admin_setting',
+			'Test Input',
+			array( $this, 'oc_render_settings_field' ),
+			'oc_general_settings',
+			'oc_general_section',
+			$args
+		);
+
+
+		register_setting(
+			'oc_general_settings',
+			'oc_admin_setting'
+		);
+
+	}
+
+	public function oc_display_general_account() {
+		echo '<p>These settings apply to all Odds Comparison Plugin functionality.</p>';
+	}
+
+	public function oc_render_settings_field($args) {  
+	if($args['wp_data'] == 'option'){
+	   $wp_data_value = get_option($args['name']);
+	} elseif($args['wp_data'] == 'post_meta'){
+		$wp_data_value = get_post_meta($args['post_id'], $args['name'], true );
+	}
+
+   switch ($args['type']) {
+	   case 'input':
+			$value = ($args['value_type'] == 'serialized') ? serialize($wp_data_value) : $wp_data_value;
+			if($args['subtype'] != 'checkbox'){
+				$prependStart = (isset($args['prepend_value'])) ? '<div class="input-prepend"> <span class="add-on">'.$args['prepend_value'].'</span>' : '';
+				$prependEnd = (isset($args['prepend_value'])) ? '</div>' : '';
+				$step = (isset($args['step'])) ? 'step="'.$args['step'].'"' : '';
+				$min = (isset($args['min'])) ? 'min="'.$args['min'].'"' : '';
+				$max = (isset($args['max'])) ? 'max="'.$args['max'].'"' : '';
+				if(isset($args['disabled'])){
+					// hide the actual input bc if it was just a disabled input the informaiton saved in the database would be wrong - bc it would pass empty values and wipe the actual information
+					echo $prependStart.'<input type="'.$args['subtype'].'" id="'.$args['id'].'_disabled" '.$step.' '.$max.' '.$min.' name="'.$args['name'].'_disabled" size="40" disabled value="' . esc_attr($value) . '" /><input type="hidden" id="'.$args['id'].'" '.$step.' '.$max.' '.$min.' name="'.$args['name'].'" size="40" value="' . esc_attr($value) . '" />'.$prependEnd;
+				} else {
+					echo $prependStart.'<input type="'.$args['subtype'].'" id="'.$args['id'].'" "'.$args['required'].'" '.$step.' '.$max.' '.$min.' name="'.$args['name'].'" size="40" value="' . esc_attr($value) . '" />'.$prependEnd;
+				}
+				/*<input required="required" '.$disabled.' type="number" step="any" id="'.$this->plugin_name.'_cost2" name="'.$this->plugin_name.'_cost2" value="' . esc_attr( $cost ) . '" size="25" /><input type="hidden" id="'.$this->plugin_name.'_cost" step="any" name="'.$this->plugin_name.'_cost" value="' . esc_attr( $cost ) . '" />*/
+
+			} else {
+				$checked = ($value) ? 'checked' : '';
+				echo '<input type="'.$args['subtype'].'" id="'.$args['id'].'" "'.$args['required'].'" name="'.$args['name'].'" size="40" value="1" '.$checked.' />';
+			}
+		break;
+	   default:
+		   # code...
+		break;
+    }
+}
 
 }
--- wp-content/plugins/odds-comparison/admin/partials/oc-admin-display.php
+++ wp-content/plugins/odds-comparison/admin/partials/oc-admin-display.php
@@ -14,7 +14,16 @@
 ?>
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
-
 <div class="wrap">
-    <h2>Odds Comparison Plugin Settings</h2>
+    <div id="icon-themes" class="icon32"></div>  
+    <h2>Plugin Name Settings</h2>  
+        <!--NEED THE settings_errors below so that the errors/success messages are shown after submission - wasn't working once we started using add_menu_page and stopped using add_options_page so needed this-->
+    <?php settings_errors(); ?>  
+    <form method="POST" action="options.php">  
+        <?php 
+            settings_fields( 'oc_general_settings' );
+            do_settings_sections( 'oc_general_settings' ); 
+        ?>             
+        <?php submit_button(); ?>  
+    </form> 
 </div>
