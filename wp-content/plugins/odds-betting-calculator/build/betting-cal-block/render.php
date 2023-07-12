8a1f7b55b74bc1ae662d2deda399e66a200a182f
awwm <wahab83.pk@gmail.com>
2023-07-12 16:41:21 +0200

    betting calculator block fields

--- wp-content/plugins/odds-betting-calculator/build/betting-cal-block/render.php
+++ wp-content/plugins/odds-betting-calculator/build/betting-cal-block/render.php
@@ -3,6 +3,65 @@
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
 ?>
-<p <?php echo get_block_wrapper_attributes(); ?>>
-	<?php esc_html_e( 'Betting Calculator – hello from a dynamic block!', 'ocbc' ); ?>
-</p>
+<section <?php echo get_block_wrapper_attributes(); ?>>
+	<?php esc_html_e( 'Betting Calculator', 'ocbc' ); ?>
+
+	<div class="results-group">
+		<fieldset>
+			<legend>Bet Slip</legend>
+			<div class="results">
+				<div class="expected-winnings">
+					<span><?php esc_html_e( 'Expected Winnings', 'ocbc' ); ?></span>
+					<span>
+						<div class="result-values">
+							<sup><?php esc_html_e( '$', 'ocbc' ); ?></sup>
+							<span id="winning" class="results"></span>
+						</div>
+					</span>
+				</div>
+				<div class="expected-payout">
+					<span><?php esc_html_e( 'Expected Payout', 'ocbc' ); ?></span>
+					<span>
+						<div class="result-values">
+							<sup><?php esc_html_e( '$', 'ocbc' ); ?></sup>
+							<span id="payout" class="results"></span>
+						</div>
+					</span>
+				</div>
+			</div>
+		</fieldset>
+	</div>
+
+	<div class="form-wrapper">
+		<div class="fields-group">
+			<fieldset>
+				<legend>Create Wager</legend>
+				<div>
+					<label for="calc-bet-amount">Bet Amount</label>
+					<input type="text" maxlength="10" minlength="2" id="calc-bet-amount" name="calc-bet-amount" placeholder="ie, $100" pattern="\$?[0-9]{1,5}" value="">
+				</div>
+				<div>
+					<label for="calc-moneyline">American Odds</label>
+					<input type="text" maxlength="7" minlength="3" id="calc-moneyline" name="calc-moneyline" placeholder="ie, -110" pattern="[-+]{0,1}[0-9]{3}" value="">
+				</div>
+			</fieldset>
+		</div>
+
+		<div class="fields-group">
+			<fieldset>
+				<div>
+					<label for="calc-fractional">Fractional Odds</label>
+					<input type="text" maxlength="6" minlength="3" id="calc-fractional" name="calc-fractional" placeholder="ie, 4/1" pattern="[0-9]{1,4}/[0-9]{1,2}" value="">
+				</div>
+				<div>
+					<label for="calc-decimal">Decimal</label>
+					<input type="text" maxlength="7" minlength="1" id="calc-decimal" name="calc-decimal" placeholder="ie, 2.0" pattern="^[0-9]{1,4}(.[0-9]+)?$" value="">
+				</div>
+				<div>
+					<label for="calc-implied">Implied Odds</label>
+					<input type="text" maxlength="5" minlength="2" id="calc-implied" name="calc-implied" placeholder="ie, 50%" pattern="[0-9]{1,3}(.[0-9]{1,2})?%?" value="">
+				</div>
+			</fieldset>
+		</div>
+	</div>
+</section>
--- wp-content/plugins/odds-betting-calculator/build/odds-comp-block/index.asset.php
+++ wp-content/plugins/odds-betting-calculator/build/odds-comp-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'c7d0a41a7b22ab47ca42');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '429f152c966f737ca5a0');
