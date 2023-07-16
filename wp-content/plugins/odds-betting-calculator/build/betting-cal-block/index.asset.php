314b626adcd2da311a35f79b585b6413729d0682
awwm <wahab83.pk@gmail.com>
2023-07-16 21:40:39 +0200

    updated build version

--- wp-content/plugins/odds-betting-calculator/build/betting-cal-block/index.asset.php
+++ wp-content/plugins/odds-betting-calculator/build/betting-cal-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '8c1a5789311588e05e4e');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'aec74c909efdf75fbea4');
--- wp-content/plugins/odds-betting-calculator/build/odds-comp-block/index.asset.php
+++ wp-content/plugins/odds-betting-calculator/build/odds-comp-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e7809263b3c46d94d2c6');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '684cc15da2a464f2d93e');
